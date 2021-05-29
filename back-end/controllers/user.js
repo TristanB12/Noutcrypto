985ab0bfd5fecda98894db71f53950a56d74f5d7
Julien PAUSE <julien.pause@epitech.eu>
2021-05-29 21:27:32 +0400

    feat: add name to user model

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -6,12 +6,13 @@ module.exports = {
         bcrypt.hash(req.body.password, 10)
             .then(hash => {
                 const user = new User({
+                    name: req.body.name,
                     email: req.body.email,
                     password: hash,
-                    wallet: 0
+                    balance: 0
                 })
                 user.save()
-                    .then(() => res.status(201).json({ message: 'User created' }))
+                    .then(() => res.status(201).json(user))
                     .catch(err => res.status(400).json({ err }))
             })
             .catch(err => res.status(500).json({ err }))

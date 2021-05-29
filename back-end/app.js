b4b182405d6af66f6466d9b086e5a16c41b83140
Julien PAUSE <julien.pause@epitech.eu>
2021-05-29 17:50:33 +0400

    feat: add user auth

---
+++ back-end/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const result = require('dotenv').config()
+if (result.error) {
+    throw result.error
+}
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_DB_URI,
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Successfully connected to MongoDB !'))
+    .catch(() => console.log('Failed connecting to MongoDB'));
+
+const app = express()
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+app.use(bodyParser.json());
+
+const userRoutes = require('./routes/user')
+
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
---
+++ back-end/controllers/user.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+module.exports = {
+    signup(req, res, next) {
+        bcrypt.hash(req.body.password, 10)
+            .then(hash => {
+                const user = new User({
+                    email: req.body.email,
+                    password: hash
+                })
+                user.save()
+                    .then(() => res.status(201).json({ message: 'User created' }))
+                    .catch(err => res.status(400).json({ err }))
+            })
+            .catch(err => res.status(500).json({ err }))
+    },
+
+    login(req, res, next) {
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    return res.status(401).json({ error: 'User not found' })
+                }
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (!valid) {
+                            return res.status(401).json({ error: 'Incorrect password' })
+                        }
+                        return res.status(200).json(user)
+                    })
+                    .catch(err => res.status(500).json({ err }))
+            })
+            .catch(err => res.status(500).json({ err }))
+    }
+}

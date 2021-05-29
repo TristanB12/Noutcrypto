7238124815feb3d6d333ffb3bac1fed376c2ad39
Julien PAUSE <julien.pause@epitech.eu>
2021-05-30 01:47:18 +0400

    update imagePath from CryptoFarm model

--- back-end/app.js
+++ back-end/app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 const userRoutes = require('./routes/user')
 const farmRoutes = require('./routes/cryptoFarm')
 
-app.use('/api/auth', userRoutes);
+app.use('/api/auth', userRoutes)
 app.use('/api/farm', farmRoutes)
 
 module.exports = app;
--- back-end/models/CryptoFarm.js
+++ back-end/models/CryptoFarm.js
@@ -5,7 +5,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const cryptoFarmSchema = mongoose.Schema({
   name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
-  image_path: { type: String, required: true },
+  imagePath: { type: String, required: true },
   funding: { type: Number, required: true },
   fundingGoal: { type: Number, required: true }
 })

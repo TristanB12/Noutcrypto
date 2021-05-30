6fab259570b3e6bc787dcc256057ab153c739318
tristanB12 <tristan.bourgeois@epitech.eu>
2021-05-30 09:33:35 +0400

    test video

--- front-end/src/views/FarmsPage.vue
+++ front-end/src/views/FarmsPage.vue
@@ -33,7 +33,7 @@ import axios from 'axios'
         },
         methods: {
             getFarms() {
-                axios.get('http://localhost:8081/api/farm/')
+                axios.get('/api/farm/')
                     .then(response => this.farms = response.data)
                     .catch(err => console.log(err))
             },

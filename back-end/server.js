4d5cba69c288e94e29494753f668683c26bff0d2
Julien PAUSE <julien.pause@epitech.eu>
2021-06-02 12:04:24 +0000

    fix(backend): listen on localhost

--- back-end/server.js
+++ back-end/server.js
@@ -39,10 +39,10 @@ const errorHandler = (error) => {
 const server = http.createServer(app);
 
 server.on("error", errorHandler);
+server.listen(port, "localhost");
 server.on("listening", () => {
   const address = server.address();
   const bind = typeof address === "string" ? "pipe " + address : "port " + port;
   console.log("Listening on " + bind);
 });
 
-server.listen(port);
